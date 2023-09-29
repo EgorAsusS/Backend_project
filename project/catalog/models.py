40cc9b903026f8cc1ecafdcb8afdfa3e8de632f2
EgorAsusS <egor.firsov.egor@yandex.ru>
2023-09-29 14:59:14 +0700

    Добавил классы товара и корзины

--- project/catalog/models.py
+++ project/catalog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models import F
 
-# Create your models here.
+
+class Item(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+
+
+class Basket(models.Model):
+    quantity = models.IntegerField()
+    total_price = F('Item.price') * F('quantity')
+
