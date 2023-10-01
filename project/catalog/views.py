19585e9f5d6f4dbe09e9ab1c00560fc925624b7b
EgorAsusS <egor.firsov.egor@yandex.ru>
2023-10-01 17:13:02 +0700

    Сделал полную регистрация

--- project/catalog/views.py
+++ project/catalog/views.py
@@ -19,22 +19,30 @@ def index(request):
         firstname = form.cleaned_data.get("first_name")
         lastname = form.cleaned_data.get("last_name")
         emailvalue = form.cleaned_data.get("email")
-        for i in users:
-            if i.username == username:
-                return JsonResponse({'Status': 403,
-                                     'Content-Type': 'application/json',
-                                     'Body':
-                                         {
-                                             'message': 'Login failed'
-                                         }
-                                     })
-            else:
-                user = User.objects.create_user(username)
-                user.first_name = firstname
-                user.last_name = lastname
-                user.email = emailvalue
-                user.save()
-
+        if User.objects.filter(username=username).first():
+            return JsonResponse({'Status': 403,
+                                'Content-Type': 'application/json',
+                                'Body':
+                                    {
+                                        'message': 'Login failed'
+                                    }
+                                })
+        else:
+            user = User.objects.create_user(username)
+            user.first_name = firstname
+            user.last_name = lastname
+            user.email = emailvalue
+            user.save()
+            return JsonResponse({'Status': 201,
+                                 'Content-Type': 'application/json',
+                                 'Body':
+                                     {
+                                        'username': username,
+                                        'first_name': firstname,
+                                        'last_name': lastname,
+                                        'email': emailvalue
+                                     }
+                                 })
     return render(request, 'catalog/index.html', {'form': form})
 
 
