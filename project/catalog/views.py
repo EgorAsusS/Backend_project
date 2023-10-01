7778734e485a2d16aa294ef1de4600eee126186f
EgorAsusS <egor.firsov.egor@yandex.ru>
2023-10-01 08:46:22 +0700

    Revert "Добавил форму для пользователя"
    This reverts commit 93bf803c7e941cd6b43629d5c30b64195b6fc862.

--- project/catalog/views.py
+++ project/catalog/views.py
@@ -1,15 +1,48 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.models import User
-# Create your views here.
-def index(request):
-    return render(request, 'catalog/form_register.html')
+from .forms import UserForm
 
+def index(request):
+    submitbutton = request.POST.get("submit")
+
+    username = ''
+    firstname = ''
+    lastname = ''
+    emailvalue = ''
+
+    users = User.objects.all()
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        firstname = form.cleaned_data.get("first_name")
+        lastname = form.cleaned_data.get("last_name")
+        emailvalue = form.cleaned_data.get("email")
+        for i in users:
+            if i.username == username:
+                return JsonResponse({'Status': 403,
+                                    'Content-Type': 'application/json',
+                                    'Body':
+                                        {
+                                            'message': 'Login failed'
+                                        }
+                                    })
+            else:
+                user = User.objects.create_user(username)
+                user.first_name = firstname
+                user.last_name = lastname
+                user.email = emailvalue
+                user.save()
+    context = {
+        'username': username,
+        'first_name': firstname,
+        'last_name': lastname,
+        'email': emailvalue,
+    }
+    return render(request, 'catalog/index.html', context)
 
 
 def register(request):
-    user = User.objects.create_user('myusername', 'myemail@crazymail.com', 'mypassword')
-    user.first_name = 'John'
-    user.last_name = 'Citizen'
+    user = User.objects.create_user(username, user_email, user_password, user_first_name, user_last_name)
     user.save()
     return HttpResponse('Пользователь создан')
